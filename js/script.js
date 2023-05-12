ce5b8475873662a03c9da5d39a773045e3e1c27f
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-12 15:36:31 +0200

    calculating price and printing it in html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let userAge = parseInt(prompt("Inserisci qui la tua età"));
-let userJourney = parseInt(prompt("Inserisci qui la quantità di km che vuoi percorrere"))
+let userJourney = parseInt(prompt("Inserisci qui la quantità di km che vuoi percorrere"));
 
-document.getElementById("yourAgeIs").innerHTML += userAge + " anni"
-document.getElementById("yourJourneyIs").innerHTML += userJourney + " km"
+document.getElementById("yourAgeIs").innerHTML += userAge + " anni";
+document.getElementById("yourJourneyIs").innerHTML += userJourney + " km";
+
+const ticketCost = 0.21 * userJourney;
+
+document.getElementById("ticketPrice").innerHTML += ticketCost
