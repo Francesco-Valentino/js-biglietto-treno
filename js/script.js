c76ada4f576bfd94f9efd582ab3de4f1918cb756
Francesco-Valentino <francesco.valentino.1993@gmail.com>
2023-05-12 15:29:53 +0200

    setting prompt for numeric values insertion (age and distance)

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let userAge = parseInt(prompt("Inserisci qui la tua età"));
+let userJourney = parseInt(prompt("Inserisci qui la quantità di km che vuoi percorrere"))
+
+document.getElementById("yourAgeIs").innerHTML += userAge + " anni"
+document.getElementById("yourJourneyIs").innerHTML += userJourney + " km"
